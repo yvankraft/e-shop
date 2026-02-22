e65c8172f8223380b6b74d115c1db212adc03e48
yvankraft <yvanngone53@gmail.com>
2026-02-22 13:00:14 +0100

    Remove unnecessary classes from the main container in the Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "@/app/components/Navbar";
 
 export default function Home() {
   return (
-    <div className="container flex flex-col items-center justify-center">
+    <div >
       <Navbar />
       <h1>Welcome to our E-shop</h1>
     </div>
